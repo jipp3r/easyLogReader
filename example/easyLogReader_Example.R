57217dfd09a924d08bdc62d3b4b4373ed2f137ac
jipp3r <36063296+jipp3r@users.noreply.github.com>
2018-03-23 20:39:49 +0200

    Analyse data from Easylog

--- example/easyLogReader_Example.R
+++ example/easyLogReader_Example.R
@@ -6,7 +6,7 @@ library(easyLogReader)
 x <- read.easyLogBatch("data", "output", "output/images", noimage=TRUE)
 
 # Processes each file in the 'data' folder, using only the first 24 hours of each trace
-y <- read.easyLogBatch("data", "output", "output/images", noimage=TRUE, leadtime = 24, truncate = 24)
+y <- read.easyLogBatch("data", "output", "output/images", noimage=TRUE, leadtime = 5, truncate = 5)
 
 # Writes out results to CSV
 write.csv(y, "output/AnalysedResults.csv", row.names = TRUE)
